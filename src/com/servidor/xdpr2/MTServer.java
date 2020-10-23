60f90ea6bac4cdd0f77465fe08c1c7ff2fb3e014
Josep Bello Curto <josep.bello@estudiants.urv.cat>
2020-10-23 14:03:15 +0200

    Servidor multi thread

---
+++ src/com/servidor/xdpr2/MTServer.java
@@ -0,0 +1,30 @@
+package com.servidor.xdpr2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MTServer {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(1234);
+        } catch (IOException e) {
+            System.out.println("Error al fer la connexio");
+            e.printStackTrace();
+        }
+
+        Socket connectionSocket = null;
+
+        for (;;) {
+            try {
+                connectionSocket = serverSocket.accept();
+                new AttendPetition(connectionSocket).start();
+            } catch (IOException e) {
+                System.out.println("Error al aceptar la peticio");
+                e.printStackTrace();
+            }
+        }
+    }
+}
