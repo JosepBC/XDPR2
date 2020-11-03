54d6c0ce933c3c4a254a14d1ee7752774ea27954
Josep Bello Curto <josep.bello@estudiants.urv.cat>
2020-11-03 20:47:16 +0100

    Pruebas base de datos

--- src/xdpr2/servidor/Main.java
+++ src/xdpr2/servidor/Main.java
@@ -1,8 +1,31 @@
 package xdpr2.servidor;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hola");
+    public static void main(String[] args) throws Exception {
+        List<Message> listMsg = new LinkedList<>();
+
+        listMsg.add(new Message("A", 2, 2, 2, 2 ));
+        listMsg.add( new Message("A", 2, 2, 2,2 ));
+        listMsg.add( new Message("A", 2, 2, 2,2 ));
+        listMsg.add( new Message("A", 7, 2, 2,2 ));
+
+        listMsg.add( new Message("B", 2, 2, 2,2 ));
+        listMsg.add( new Message("B", 2, 2, 2,2 ));
+        listMsg.add( new Message("B", 2, 2, 2,2 ));
+
+        DataBase db = new DataBase();
+        for(Message msg : listMsg) {
+            db.updateDataOfSanitaryRegion(msg);
+        }
+
+        System.out.println(db.getAvgLast24h("A"));
+        System.out.println(db.getAvgLast24h("B"));
+        System.out.println(db.getAvgLast24h("C"));
+
+        db.close();
     }
 }
