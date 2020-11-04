bdab08573a68ad572ee686041d38a279e19ed737
Josep Bello Curto <josep.bello@estudiants.urv.cat>
2020-11-04 19:52:53 +0100

    Modificacion del cliente para usar mensajes genericos

--- src/xdpr2/client/Client.java
+++ src/xdpr2/client/Client.java
@@ -1,20 +1,25 @@
 package xdpr2.client;
 
+import xdpr2.common.Message;
+
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-        Socket connection = new Socket("localhost", 1234);
+        Socket connection = new Socket(InetAddress.getLocalHost(), 1234);
         /**Read of socket*/
         ObjectOutputStream toServer = new ObjectOutputStream(connection.getOutputStream());
         ObjectInputStream from_server = new ObjectInputStream(connection.getInputStream());
         readData(toServer);
-        Message avg = (Message) from_server.readObject();
+        Message<Float> avg = (Message) from_server.readObject();
         System.out.println("Average received from server:");
         System.out.println(avg);
         connection.close();
+        toServer.close();
+        from_server.close();
     }
 
     /**
@@ -37,8 +42,9 @@ public class Client {
             newInputs = keyboard.nextInt();
             System.out.println("Enter number of new realises ICU: ");
             newOutputs = keyboard.nextInt();
-            Message message = new Message(region, newPositives, deaths, newInputs, newOutputs);
+            Message<Integer> message = new Message(region, newPositives, deaths, newInputs, newOutputs);
             toServer.writeObject(message);
+            keyboard.nextLine();
             System.out.println("Enter sanitary region or -1 to exit: ");
             region = keyboard.nextLine();
         }
